337aa2094f25cc1079f85d5d1c8e80b9c89c6bad
vaibh <vaibhavthapli123@gmail.com>
2023-10-17 16:50:36 +0530

    type_casting

---
+++ type_casting.py
@@ -0,0 +1,29 @@
+# Type_casting is also known as type_conversion
+
+#integer
+x= 50
+print(x,type(x)) #check the type of x
+
+#float
+y=1.23
+print(y,type(y))
+
+#string
+name = "Vaibhav Thapli"
+print(name,type(name))
+
+#bool
+cast = True
+print(cast,type(cast))
+
+#list
+marks = [1,2,3,4,5,6]
+print(marks,type(marks))
+
+#tuples
+av = (1,2,3,4,5,6)
+print(av,type(av))
+
+#dictionary
+vt = {"name": "vaibhav", "age": 26}
+print(vt,type(vt ))
