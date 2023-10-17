513bb5c8796b386a8837cb9cb44669a61af47439
vaibh <vaibhavthapli123@gmail.com>
2023-10-17 12:57:41 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,26 @@
+"""Python variables are simply containers for storing data values. Unlike other languages, such as Java.
+Python has no command for declaring a variable, so we create one the moment we first assign a value to it.
+"""
+# Ex:- a= 12 , c = 20, d ="Vaibhav"
+# here a,c & d are variable that stores the values.
+
+x=5
+y=23
+z = x+y
+name = "Vaibhav"
+print(x)
+print(y)
+print(z)
+print(name)
+print(x,y,z,name)
+
+""" Rules to name a Varaible:
+1. Variable names must start with a letter or an underscore(_).
+2. Variable name can only contain letters,numbers and underscores (_). Special characters and spaces are allowed.
+3. Variable name are case-sesitive (myName and myname are treated as different variables).
+4. Avoid using Python keywords and built-in function name as variable names(eg: print,if,for,while etc)
+5. Variable names should be descriptive and indicate the purpose of the variable (eg: name, school, class, etc.)
+6. It's recommended to use lowercase letters for variables names, and for names with multiple words,use underscores to seprate them(snake_case)
+"""
+
+
