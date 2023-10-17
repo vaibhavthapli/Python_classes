3aa126fc6e8df26e197098ade3fdb8aa3d4c8630
vaibh <vaibhavthapli123@gmail.com>
2023-10-17 13:19:20 +0530

    data_types

---
+++ data_types.py
@@ -0,0 +1,14 @@
+"""Data Types specifies which types of values a variable has and what mathematical relational,
+or logical operations can be performed on it without causing an error.
+"""
+
+"""Tyes of Python Data Types:
+We have basically 4 types of datatypes.
+1. Numeric Type  2. Dictionary  3. Sequence Type  4. Set
+There are sub datatypes:
+
+Numeric:- 1. Integers  2. Floats  3. Complex
+Sequence:- 1. List 2. Tuple 3. Strings
+
+When we see on internet we got :- Integer, Float, Complex, Dictionary,List,Tuples, Strings & Set
+"""
