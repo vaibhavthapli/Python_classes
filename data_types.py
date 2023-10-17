7847daaede3660d71321edb45343832db6453b87
vaibh <vaibhavthapli123@gmail.com>
2023-10-17 16:15:08 +0530

    data_types

--- data_types.py
+++ data_types.py
@@ -2,13 +2,23 @@
 or logical operations can be performed on it without causing an error.
 """
 
-"""Tyes of Python Data Types:
+"""Types of Python Data Types:
 We have basically 4 types of datatypes.
-1. Numeric Type  2. Dictionary  3. Sequence Type  4. Set
+1. Numeric Type  2. Dictionary  3. Sequence Type  4. Set 5. Boolean
 There are sub datatypes:
 
 Numeric:- 1. Integers  2. Floats  3. Complex
 Sequence:- 1. List 2. Tuple 3. Strings
 
-When we see on internet we got :- Integer, Float, Complex, Dictionary,List,Tuples, Strings & Set
+When we see on internet we got :- Integer, Float, Complex, Dictionary,List,Tuples,Boolean, Strings & Set
+
+integer:- 1,2,3
+float:- 1.2,2.3
+complex = 5i+6j
+Dictionary : {"a":123,"b":"VAibhav} Key: Value 
+List= [1,2,3,4,5]
+Tuples = (1,2,3,4)
+Strings = "VAibhav", 'Thapli',"123"
+Set:  a = {"apple", "banana", "cherry"}
+Boolean : True, False
 """
