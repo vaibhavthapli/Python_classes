fe92c729bb7fa5cac219d37cda67b7505e9a7aa0
vaibh <vaibhavthapli123@gmail.com>
2023-10-17 12:02:31 +0530

    Comment

---
+++ basic.py
@@ -0,0 +1,12 @@
+# Comments are hints that we use to make our code more understandable.
+# Comments are  completely ignored by the interpreter.
+print("Types of comments")
+print("1: Single Line Comment")
+# Hello World
+# (# is use for single line) (press Ctrl + /) for single line comment
+
+print("2: Multi Line Comments")
+
+""" Hello, I am Vaibhav Singh Thapli.
+I am a AI Developer.
+With 3 years Experience."""
